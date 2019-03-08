0f68ce1b2cac0661cc86a4c22680833b7a12d2b2
teodorstrut@yahoo.com <teodorstrut@yahoo.com>
2019-03-08 13:24:07 +0200

    created alphabetical sort for clients method for clientService

---
+++ src/main/java/Console/Command/SortClientCommand.java
@@ -0,0 +1,23 @@
+package Console.Command;
+
+import Service.ClientService;
+
+import java.util.List;
+
+public class SortClientCommand extends Command {
+    private ClientService clientService;
+
+    public SortClientCommand(ClientService cliserv){
+        this.clientService=cliserv;
+    }
+
+    @Override
+    public void execute(List<String> params){
+        clientService.sortAlfa().forEach(System.out::println);
+    }
+
+    @Override
+    public Integer paramNr(){
+        return 0;
+    }
+}
