784e240d2c474f477f6bfeec357d6aabc96e0c81
matt <stroiamatei@gmail.com>
2019-03-18 14:19:37 +0200

    fix main

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,35 +1,27 @@
 package Main;
 
-import Model.Client;
-import Model.Movie;
+import Console.Console;
 import Model.Rental;
-import Repository.FileConverter.ClientFileConverter;
-import Repository.FileConverter.MovieFileConverter;
-import Repository.Repository;
 import Repository.XMLConverter.XMLClientConverter;
 import Repository.XMLConverter.XMLMovieConverter;
 import Repository.XMLConverter.XMLRentalConverter;
+import Repository.XmlRepository;
 import Service.ClientService;
 import Service.MovieService;
-import Console.Console;
-import Repository.FileRepository;
 import Service.RentalService;
 import Validation.ClientValidator;
 import Validation.MovieValidator;
-import Repository.XmlRepository;
 import Validation.RentalValidator;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.text.ParseException;
-import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         MovieService movieService = new MovieService(new XmlRepository<>(new MovieValidator(), new XMLMovieConverter(), "src\\main\\java\\Files\\xmlmovies.xml"));
         ClientService clientService = new ClientService(new XmlRepository<>(new ClientValidator(), new XMLClientConverter(), "src\\main\\java\\Files\\xmlclients.xml"));
-        RentalService rentalService = new RentalService(new Repository<>(new RentalValidator()));
+        RentalService rentalService = new RentalService(new XmlRepository<>(new RentalValidator(), new XMLRentalConverter(), "src\\main\\java\\Files\\xmlrentals.xml"), clientService, movieService);
 
         Console c = new Console(movieService, clientService, rentalService);
 //        movieService.addMovie(1,"The Fateful Eight", LocalDate.parse("2016-01-15"));
